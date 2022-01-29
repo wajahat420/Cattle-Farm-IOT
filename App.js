719004e8795f1c4b3f8cfa69330febe9ff08ab05
Wajahat Sarwat <42533972+wajahat420@users.noreply.github.com>
2022-01-29 15:56:22 +0500

    changes

---
+++ App.js
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler'
+import { View, Text } from 'react-native';
+import React from 'react';
+import Stack from './source/navigation/stack';
+
+export default function App() {
+  return (
+    <Stack/>
+  );
+}
---
+++ source/config/firebase.js
@@ -0,0 +1,21 @@
+import firebase from '@react-native-firebase/app';
+// import storage from '@react-native-firebase/storage';
+import database from "@react-native-firebase/database"
+
+
+const firebaseConfig = {
+        apiKey: "AIzaSyCDdmrYEd2Yf8d6zW253CEfpTnPmUXV_BA",
+        authDomain: "iotbase-5879d.firebaseapp.com",
+        databaseURL: "https://iotbase-5879d-default-rtdb.firebaseio.com",
+        projectId: "iotbase-5879d",
+        storageBucket: "iotbase-5879d.appspot.com",
+        messagingSenderId: "608402415938",
+        appId: "1:608402415938:web:f9d90de99e82097004e8db",
+        measurementId: "G-0J2PGM4E5K"
+};
+        
+let Firebase = firebase.initializeApp(firebaseConfig);
+        // Initialize Firebase
+export default () => {
+        return {Firebase, database}
+};
